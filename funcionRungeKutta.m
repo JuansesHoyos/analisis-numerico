20818ebddae2e3ee1a62cd605f416d86c0d0f8af
JuansesHoyos <101542674+JuansesHoyos@users.noreply.github.com>
2024-11-20 00:21:44 -0500

    Update funcionRungeKutta.m
    Método actualizado

--- funcionRungeKutta.m
+++ funcionRungeKutta.m
@@ -1,6 +1,5 @@
-function [M2, M5, M4] = funcionRungeKutta()
-    % FUNCIONRUNGEKUTTA Método de Runge-Kutta de 3er orden para resolver ecuaciones diferenciales.
-    % Solicita los datos necesarios por consola y realiza los cálculos iterativos, además de graficar los resultados.
+function [M2, M5, M4] = funcionRungeKuttaOrden4()
+    % FUNCIONRUNGEKUTTAORDEN4 Método de Runge-Kutta de 4to orden para resolver ecuaciones diferenciales.
     %
     % Salidas:
     %   - M2: Tabla con los valores intermedios en cada iteración.
@@ -40,7 +39,7 @@ function [M2, M5, M4] = funcionRungeKutta()
     % Inicializar variables.
     x0 = x;
     y0 = y;
-    k1 = 0; k2 = 0; k3 = 0;
+    k1 = 0; k2 = 0; k3 = 0; k4 = 0;
     yii = 0;
     xi = 0;
 
@@ -48,45 +47,50 @@ function [M2, M5, M4] = funcionRungeKutta()
     M5 = zeros(1, n); % Vector de valores de x.
     M4 = zeros(1, n); % Vector de valores de y.
 
-    % Iteraciones del método de Runge-Kutta de 3er orden.
+    % Iteraciones del método de Runge-Kutta de 4to orden.
     for i = 1:n
-        yiSiguiente = yii;
         if i == 1
             % Primera iteración.
             k1 = f(x0, y0);
             xk2 = x0 + (1/2) * h;
             yk2 = y0 + (1/2) * k1 * h;
             k2 = f(xk2, yk2);
-            xk3 = x0 + h;
-            yk3 = y0 - k1 * h + 2 * k2 * h;
+            xk3 = x0 + (1/2) * h;
+            yk3 = y0 + (1/2) * k2 * h;
             k3 = f(xk3, yk3);
+            xk4 = x0 + h;
+            yk4 = y0 + k3 * h;
+            k4 = f(xk4, yk4);
 
             % Calcular el siguiente valor de y.
-            yii = y0 + (h / 6) * (k1 + 4 * k2 + k3);
+            yii = y0 + (h / 6) * (k1 + 2 * k2 + 2 * k3 + k4);
             xi = x0;
 
             % Guardar resultados.
-            M2 = num2cell([i, k1, xk2, yk2, k2, xk3, yk3, k3, yii]);
+            M2 = num2cell([i, k1, xk2, yk2, k2, xk3, yk3, k3, xk4, yk4, k4, yii]);
             M5(i) = xi;
             M4(i) = yii;
         else
             % Iteraciones posteriores.
             xi = xi + h;
-            yi = yiSiguiente;
+            yi = yii;
 
             k1 = f(xi, yi);
             xk2 = xi + (1/2) * h;
             yk2 = yi + (1/2) * k1 * h;
             k2 = f(xk2, yk2);
-            xk3 = xi + h;
-            yk3 = yi - k1 * h + 2 * k2 * h;
+            xk3 = xi + (1/2) * h;
+            yk3 = yi + (1/2) * k2 * h;
             k3 = f(xk3, yk3);
+            xk4 = xi + h;
+            yk4 = yi + k3 * h;
+            k4 = f(xk4, yk4);
 
             % Calcular el siguiente valor de y.
-            yii = yi + (h / 6) * (k1 + 4 * k2 + k3);
+            yii = yi + (h / 6) * (k1 + 2 * k2 + 2 * k3 + k4);
 
             % Guardar resultados.
-            M3 = num2cell([i, k1, xk2, yk2, k2, xk3, yk3, k3, yii]);
+            M3 = num2cell([i, k1, xk2, yk2, k2, xk3, yk3, k3, xk4, yk4, k4, yii]);
             M2 = [M2; M3];
             M5(i) = xi;
             M4(i) = yii;
@@ -95,7 +99,7 @@ function [M2, M5, M4] = funcionRungeKutta()
 
     % Mostrar los resultados en la consola.
     fprintf('\nTabla de resultados (M2):\n');
-    disp(cell2table(M2, 'VariableNames', {'Iteración', 'k1', 'xk2', 'yk2', 'k2', 'xk3', 'yk3', 'k3', 'yii'}));
+    disp(cell2table(M2, 'VariableNames', {'Iteración', 'k1', 'xk2', 'yk2', 'k2', 'xk3', 'yk3', 'k3', 'xk4', 'yk4', 'k4', 'yii'}));
     fprintf('Vector de valores de x (M5):\n');
     disp(M5);
     fprintf('Vector de valores de y (M4):\n');
@@ -105,7 +109,7 @@ function [M2, M5, M4] = funcionRungeKutta()
     figure;
     plot(M5, M4, '-o', 'LineWidth', 2, 'MarkerSize', 6, 'MarkerFaceColor', 'r');
     grid on;
-    title('Método de Runge-Kutta');
+    title('Método de Runge-Kutta de 4to Orden');
     xlabel('x');
     ylabel('y');
     legend('Solución aproximada');
